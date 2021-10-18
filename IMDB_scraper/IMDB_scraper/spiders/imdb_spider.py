33a1476afdaf7951a4b46d86b9325ffc8f995d27
Peter Flanders <pfflanders@gmail.com>
2021-10-18 15:15:28 -0700

    lots of changes to the scraper works now

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -12,18 +12,28 @@ class ImdbSpider(Spider):
     start_urls = ['https://www.imdb.com/title/tt9335498/']
 
     def parse(self, response):
-        yield Request('https://www.imdb.com/title/tt9335498/fullcredits', callback = self.parse)
+        yield Request('https://www.imdb.com/title/tt9335498/fullcredits', callback = self.parse_full_credits)
         
 
     def parse_full_credits(self, response):
-        links = [a.attrib["href"] for a in response.css("td.primary_photo a")]
-        for link in links:
-            yield Request('https://www.imdb.com/title/tt9335498/fullcredits', callback = self.parse_full_credits)
+        rel_links = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+        for link in rel_links:
+            yield Request(urljoin(response.url, link), callback = self.parse_actor_page)
 
 
     def parse_actor_page(self, response):
         # element.css("::attr(id)")
         # element.css("div.filmo-row")
         # element.css("a::text")
-        pass
+        
+        
+        name = response.css('div.name-overview span::text').get()
+        
+        movie_list = [item.css('a::text').get() for item in response.css('div.filmo-row')]
+
+        for movie in movie_list:
+            yield {
+                "actor": name,
+                "movie_or_TV_name": movie
+            }
 
